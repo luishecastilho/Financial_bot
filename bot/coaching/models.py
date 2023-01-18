bb8d45ae224cd307487a9d831a3215197b7c5412
Luís Henrique Castilho <luishecastilho@gmail.com>
2023-01-17 21:01:42 -0300

    enviando os videos pro front

--- bot/coaching/models.py
+++ bot/coaching/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Video(models.Model):
+    title = models.CharField(max_length=200, default='')
+    description = models.CharField(max_length=500, default='')
+    url = models.CharField(max_length=200, default='')
+    duration = models.PositiveIntegerField(default=0)
+    order = models.PositiveIntegerField(default=0)
+
+    def __str__(self) -> str:
+        return self.title
--- bot/coaching/views.py
+++ bot/coaching/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Video
 
 # Create your views here.
 
 def index(request):
-    #dados de um possível LOGIN
+    #dados de um possível e necessário LOGIN
     user = []
+    account = []
 
-    return render(request, 'coaching/index.html', {'user': user})
+    videos = Video.objects.all()
+
+    return render(request, 'coaching/index.html', {
+                                                    'user': user,
+                                                    'account': account,
+                                                    'videos': videos
+                                                })
